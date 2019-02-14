7e9ac6e9c3b9be6163c26b648634681036c46347
javabyraghu <javabyraghu@gmail.com>
2019-02-14 22:35:29 +0530

    First Commit

---
+++ src/main/java/org/sathyatech/app/SpringBootFirstAppApplication.java
@@ -0,0 +1,14 @@
+package org.sathyatech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstAppApplication.class, args);
+	}
+
+}
+
